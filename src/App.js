9ead55cf49288c9808082ce10f98844d9cf51a7a
avik067 <avikbiswas067@gmail.com>
2023-09-09 20:59:58 +0530

    receive data from server

--- src/App.js
+++ src/App.js
@@ -7,9 +7,9 @@ import io from 'socket.io-client';
 function App() {
    
    
-  const  [sms,setSms]= useState({currentList:["hello","hey"], currentValue :""})
+  const  [sms,setSms]= useState({currentList:[{data: "erhget", name:"Israel Hagenes",place: "Port Maxiebury"}], currentValue :"",errMsg:"",place:'',name:''})
   const [socket, setSocket] = useState(null)
-  const {currentList,currentValue} = sms
+  const {currentList,currentValue,place,name} = sms
 
   const changeState = (event) => {
     const newVal = event.target.value
@@ -18,34 +18,47 @@ function App() {
 
   const sumitToState = (event) => {
     event.preventDefault()
+    const socketInstance = io('http://localhost:5005');
+    setSocket(socketInstance);
     const message = currentValue
-
+    
   if (socket && message) {
     socket.emit('new-message', message);
+    
   }
    
-    setSms ({...sms,currentList:[...sms.currentList,currentValue],currentValue:"" })
+  
+
+    
+  
+
+    // setSms ({...sms,currentList:[...sms.currentList,currentValue],currentValue:"" })
+    // currentList:[...sms.currentList,currentValue],
   }
   
   const conncetToSer = () => {
-    const socketInstance = io('http://localhost:4000');
+    const socketInstance = io('http://localhost:5005');
     setSocket(socketInstance);
-  
+   
     // listen for events emitted by the server
   
     socketInstance.on('connection', () => {
       console.log('Connected to server');
     });
   
-    socketInstance.on('message', (data) => {
-      console.log(`Received message: ${data}`);
+    socketInstance.on('message-back', (data) => {
+      // console.log(data);
+      const jsonData = JSON.parse(data)
+      console.log(jsonData)
+      const messageData = jsonData.data
+      setSms({...sms,currentList:[...sms.currentList,messageData],currentValue:"" ,place:jsonData.place,name:jsonData.name})
     });
 
     socketInstance.on("server" ,(msg)=> {
       console.log(`from server : ${msg}`)
     })
 
-  
+    
     return () => {
       if (socketInstance) {
         socketInstance.disconnect();
@@ -56,7 +69,8 @@ function App() {
   useEffect(()=>{
     conncetToSer()
 
-  } ,[])
+  } ,[sms])
+
 
 
 
@@ -64,7 +78,7 @@ function App() {
     <div className="App">
       <div className='main-chat-box'>
          <ul>
-            {currentList.map(each => <li className='sms'>{each}</li>)}
+            {currentList.map(each => <><li className='sms'>{each.data} <br/> {`name : ${each.name}`}<br />{`place : ${each.place}`}</li></>)}
          </ul>
       </div>
       <form onSubmit={sumitToState}>
